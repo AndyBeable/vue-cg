64290045e63a4bbf328a8bef64672c910cef8467
AndyBeable <andy.beable@gmail.com>
2021-08-10 17:47:45 +0100

    assignment 5 final

---
+++ assignment_5/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredTaskValue: '',
+      tasks: [],
+      taskListIsVisible: true,
+    };
+  },
+  computed: {
+    buttonCaption() {
+      return this.taskListIsVisible ? 'Hide List' : 'Show List';
+    },
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.enteredTaskValue);
+      this.enteredTaskValue = '';
+    },
+    toggleTaskList() {
+      this.taskListIsVisible = !this.taskListIsVisible;
+    },
+  },
+});
+
+app.mount('#assignment');
