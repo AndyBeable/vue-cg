3c849bdf287fede082a1bd59fc0736a085f0b01d
AndyBeable <andy.beable@gmail.com>
2021-08-10 11:22:16 +0100

    looping

--- balloons/script.js
+++ balloons/script.js
@@ -2,6 +2,14 @@ const app1 = new Vue({
   el: '#app1',
   data: {
     message: 'Hello World',
+    countdown: [5, 4, 3, 2, 1],
+    fruits: ['Apple', 'Banana', 'Orange', 'Mango'],
+    profile: {
+      name: 'Andy',
+      age: '32',
+      degree: 'Sport Science',
+      position: 'Front End Developer',
+    },
   },
 });
 
