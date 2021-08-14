b6b03c90fd5bcbed73fdbf427361253eae65cbb7
AndyBeable <andy.beable@gmail.com>
2021-08-14 16:09:32 +0100

    inital cli setup

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Andy',
+          name: 'Andy Beable',
+          phone: '01234567678',
+          email: 'andy.beable@gmail.com',
+        },
+        {
+          id: 'Iris',
+          name: 'Iris Esteve Hernandez',
+          phone: '01234567678',
+          email: 'irisesteve@gmail.com',
+        },
+      ],
+    };
+  },
+};
+</script>
