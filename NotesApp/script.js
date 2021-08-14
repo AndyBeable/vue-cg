af4db675d0e5822329346fc764d224f465670cd9
AndyBeable <andy.beable@gmail.com>
2021-08-14 17:25:16 +0100

    add notes function and render

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -1,3 +1,16 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredNoteValue: '',
+      notes: [],
+    };
+  },
+  methods: {
+    addNote() {
+      this.notes.push(this.enteredNoteValue);
+      this.enteredNoteValue = '';
+    },
+  },
+});
 
 app.mount('#app');
