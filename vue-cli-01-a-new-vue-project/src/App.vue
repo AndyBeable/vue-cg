394125672ddee1b7b2b5f2d1e4a4b4b25e9bdb36
AndyBeable <andy.beable@gmail.com>
2021-08-15 14:29:35 +0100

    add friend func

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -19,9 +20,9 @@
 </template>
 
 <script>
-import FriendContact from './components/FriendContact.vue';
+import NewFriend from './components/NewFriend.vue';
 export default {
-  components: { FriendContact },
+  components: { NewFriend },
   data() {
     return {
       friends: [
@@ -48,6 +49,16 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavourite = !identifiedFriend.isFavourite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavourite: false,
+      };
+      this.friends.push(newFriendContact);
     },
   },
 };
@@ -86,7 +97,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -118,5 +130,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
