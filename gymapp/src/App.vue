5fc29000f5ac80a11acf033f5a73c78ed4005ca8
AndyBeable <andy.beable@gmail.com>
2021-08-24 16:16:59 +0200

    style update

--- gymapp/src/App.vue
+++ gymapp/src/App.vue
@@ -35,87 +35,17 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 * {
   box-sizing: border-box;
 }
 
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
 body {
   margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-#app li,
-#app form {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 0.5rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-  list-style: none;
-}
-
-#app input {
-  margin: 0.25rem 0rem 0.25rem 1rem;
-  padding: 0.5rem;
-}
-
-#app button {
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-  background-color: #75a5d6;
-  color: #2c3e50;
-  outline: none;
-  border: none;
-  text-transform: uppercase;
-  cursor: pointer;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-  margin: 1rem 1rem;
-}
-#app button:hover,
-#app button:active {
-  background-color: #618ebb;
-  color: #2c3e50;
-  outline: none;
-  border: none;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-
-#app .delete-btn {
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-  background-color: #f35c5c;
-  color: #2c3e50;
-  outline: none;
-  border: none;
-  text-transform: uppercase;
-  cursor: pointer;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-  margin: 1rem 1rem;
-}
-
-#app .delete-btn:hover,
-#app .delete-btn:active {
-  background-color: #ff8c8c;
-  color: #2c3e50;
-  outline: none;
-  border: none;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
 </style>
