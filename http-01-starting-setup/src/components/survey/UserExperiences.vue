7ad8d5606985a79a8300b55097bb7247daf69ad6
AndyBeable <andy.beable@gmail.com>
2021-08-27 13:06:54 +0200

    handling browser errors

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -8,10 +8,14 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">
+        {{ error }}
+      </p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">
         No stored experiences found. Start adding some survey results first.
       </p>
-      <ul v-else-if="!isLoading && results && results.length > 0">
+
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -33,12 +37,14 @@ export default {
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null
     };
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch(
         'https://vue-http-demo-e0c56-default-rtdb.firebaseio.com/surveys.json'
       )
@@ -58,6 +64,11 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch(error => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later';
         });
     }
   },
