e8536de9a0786f4dd1ba8b9390c2afac04a2023e
AndyBeable <andy.beable@gmail.com>
2021-08-27 13:23:01 +0200

    server error handling

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -55,7 +56,8 @@ export default {
     return {
       enteredName: '',
       chosenRating: null,
-      invalidInput: false
+      invalidInput: false,
+      error: null
     };
   },
   // emits: ['survey-submit'],
@@ -69,22 +71,31 @@ export default {
 
       // this.$emit('survey-submit', {
       //   userName: this.enteredName,
-      //   rating: this.chosenRating
+      //   rating: this.chosenRating,
       // });
 
-      fetch(
-        'https://vue-http-demo-e0c56-default-rtdb.firebaseio.com/surveys.json',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name: this.enteredName,
-            rating: this.chosenRating
-          })
-        }
-      );
+      this.error = null;
+      fetch('https://vue-http-demo-85e9e.firebaseio.com/surveys.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.enteredName,
+          rating: this.chosenRating
+        })
+      })
+        .then(response => {
+          if (response.ok) {
+            // ...
+          } else {
+            throw new Error('Could not save data!');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          this.error = error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
