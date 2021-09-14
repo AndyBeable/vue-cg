fad0a2637153e38a41d11a6846e5baa5caedc740
AndyBeable <andy.beable@gmail.com>
2021-09-14 15:55:40 +0100

    custom css class names

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -5,7 +5,7 @@
       <button @click="animateBlock">Animate</button>
     </div>
     <div class="container">
-      <transition>
+      <transition name="para">
         <p v-if="paraIsVisible">This is only sometimes visible...</p>
       </transition>
       <button @click="toggleParagraph">Toggle paragraph</button>
@@ -94,35 +94,35 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
 
-.v-enter-active {
-  transition: all 0.3s ease-out;
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
-.v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
-.v-leave-active {
-  transition: all 0.3s ease-in;
+.para-leave-active {
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
 }
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
