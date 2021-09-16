63a75121ed25ed624da4c2ec95450c17cb1d8603
AndyBeable <andy.beable@gmail.com>
2021-09-16 10:55:47 +0100

    mappers

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,13 +1,18 @@
 <template>
-  <button @click="addOne">Add 2</button>
+  <div>
+    <button @click="increment">Add 2</button>
+    <button @click="increase({ payload: 11 })">Add 11</button>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
-    addOne() {
-      this.$store.dispatch('increment');
-    }
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // }
+    ...mapActions(['increment', 'increase'])
   }
 };
 </script>
