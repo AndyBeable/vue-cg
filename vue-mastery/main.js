9fce862aec15a2e969db5bfe131e5e2f1dcd1872
AndyBeable <andy.beable@gmail.com>
2021-09-17 14:29:01 +0100

    computed props

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -1,9 +1,9 @@
 let app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/green-socks-on-white.jpg',
-    inStock: false,
+    selectedVariant: 0,
     onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
@@ -11,11 +11,13 @@ let app = new Vue({
         variantId: 2234,
         variantColour: 'green',
         variantImage: './assets/green-socks-on-white.jpg',
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
         variantColour: 'blue',
         variantImage: './assets/blue-socks-on-white.jpg',
+        variantQuantity: 0,
       },
     ],
     // sizes: ['6', '7', '8', '9'],
@@ -28,8 +30,25 @@ let app = new Vue({
     // removeFromCart() {
     //   this.cart--;
     // },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on Sale!';
+      }
+      return this.brand + ' ' + this.product + ' are not on Sale!';
     },
   },
 });
