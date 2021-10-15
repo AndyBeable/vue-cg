0b88dad5d2c569c4e7898e4954e5e0980845bdc1
AndyBeable <andy.beable@gmail.com>
2021-10-15 14:48:30 +0100

    counter prac

---
+++ countdown/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{ displayDays }}</div>
+    <div>{{ displayHours }}</div>
+    <div>{{ displayMinutes }}</div>
+    <div>{{ displaySeconds }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      displayDays: 0,
+      displayHours: 0,
+      displayMinutes: 0,
+      displaySeconds: 0,
+    };
+  },
+};
+</script>
