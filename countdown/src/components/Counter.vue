6082a2276b1c9558595075834d83260d2a69d257
AndyBeable <andy.beable@gmail.com>
2021-10-17 15:15:37 +0100

    computed

--- countdown/src/components/Counter.vue
+++ countdown/src/components/Counter.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="counter__container">
     <div class="counter__item">
-      {{ displayDays }}
-      <div class="label">days</div>
+      <p>{{ displayDays }}</p>
+      <div class="label">Days</div>
     </div>
     <div class="counter__item">
-      {{ displayHours }}
+      <p>{{ displayHours }}</p>
       <div class="label">Hours</div>
     </div>
     <div class="counter__item">
-      {{ displayMinutes }}
+      <p>{{ displayMinutes }}</p>
       <div class="label">Minutes</div>
     </div>
     <div class="counter__item">
-      {{ displaySeconds }}
+      <p>{{ displaySeconds }}</p>
       <div class="label">Seconds</div>
     </div>
   </div>
@@ -29,17 +29,44 @@ export default {
       displaySeconds: 0,
     };
   },
+
+  computed: {
+    seconds() {
+      return () => 1000;
+    },
+    minutes() {
+      return this.seconds * 60;
+    },
+    hours() {
+      return this.minutes * 60;
+    },
+    days() {
+      return this.hours * 24;
+    },
+  },
 };
 </script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 .counter__container {
   display: flex;
   justify-content: center;
 }
 
 .counter__item {
-  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.counter__item p {
+  font-size: 2.5rem;
 }
 
 .counter__item:not(:last-of-type) {
