8339402779ede90e592b9bb09e9edc669988536a
AndyBeable <andy.beable@gmail.com>
2021-10-21 16:21:44 +0100

    computed props

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -1,9 +1,9 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
-    image: "./assets/green-socks-on-white.jpg",
-    inStock: false,
+    selectedVariant: 0,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
@@ -11,21 +11,41 @@ var app = new Vue({
         variantId: 2234,
         variantColor: "green",
         variantImage: "./assets/green-socks-on-white.jpg",
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./assets/blue-socks-on-white.jpg",
+        variantQuantity: 0,
       },
     ],
     cart: 0,
+    onSale: true,
   },
   methods: {
     addToCart() {
       this.cart++;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " are on sale!";
+      }
+      return this.brand + " " + this.product + " are not on sale";
     },
   },
 });
