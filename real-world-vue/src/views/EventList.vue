f046ffd44780a01b55ca6a621cdcd3e83e9cdae0
AndyBeable <andy.beable@gmail.com>
2021-10-26 11:42:39 +0100

    auto register global comps

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard />
+    <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from "../components/EventCard.vue";
 export default {
+  name: "Event List",
   components: {
     EventCard,
   },
